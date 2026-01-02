ef2adb19a41bff05a93f4e7088d31ef88fb2cddc
Victor Gherardi <gherardivictor@gmail.com>
2026-01-02 01:13:55 +0100

    handle invalid commands

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
@@ -7,6 +8,14 @@ int main(int argc, char *argv[]) {
 
   // TODO: Uncomment the code below to pass the first stage
   printf("$ ");
+
+  // get user input
+  char input[256];
+  fgets(input, sizeof(input), stdin);
+
+  // handle invalid commands
+  input[strlen(input) - 1] = '\0'; // remove newline character
+  printf("%s: command not found\n", input);
 
   return 0;
 }
